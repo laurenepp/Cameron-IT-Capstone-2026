2133f0d4910809578ae5b880aa881a3a781ced0e
Michael Phillips <jasonp219@gmail.com>
2026-02-26 12:08:31 -0600

    This is going to be used for testing the Login

--- dashboard.php
+++ dashboard.php
@@ -1,2 +1,14 @@
-<?php
-?>
+users = [
+    "admin" => [
+        "password" => "Admin123!",
+        "role" => "admin"
+    ],
+    "doctor" => [
+        "password" => "Doctor123!",
+        "role" => "doctor"
+    ],
+    "nurse" => [
+        "password" => "Nurse123!",
+        "role" => "nurse"
+    ]
+];
